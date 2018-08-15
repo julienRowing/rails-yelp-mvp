dee38be346d285bb78c53bb1995e718e9f927af5
Julien Maisonnier <julien_maisonnier@yahoo.com>
2018-08-15 20:00:26 +0200

    02-Yelp MVP finished, no CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+
+
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Springroll',
+    address:      'Rue Jean',
+    phone_number:  '0101010101',
+    category:        'chinese'
+  },
+  {
+    name:         'Milanese',
+    address:      'Rue Michel',
+    phone_number:  '0202020202',
+    category:        'italian'
+  },
+  {
+    name:         'Sushis',
+    address:      'Rue Chapon',
+    phone_number:  '0303030303',
+    category:        'japanese'
+  },
+  {
+    name:         'Gigot',
+    address:      'Rue Dupuis',
+    phone_number:  '0404040404',
+    category:        'french'
+  },
+  {
+    name:         'Waffler',
+    address:      'Rue Dupont',
+    phone_number:  '0505050505',
+    category:        'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
